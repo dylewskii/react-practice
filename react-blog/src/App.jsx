da2785e73cadaf8097fb73e778aa22a07f62a564
dylewski <130579182+dylewskii@users.noreply.github.com>
2024-01-10 20:00:00 +0000

    add blog details

--- react-blog/src/App.jsx
+++ react-blog/src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Create from "./components/Create";
+import BlogDetails from "./components/BlogDetails";
 
 function App() {
   return (
@@ -10,12 +11,16 @@ function App() {
       <div className="App">
         <Navbar />
         <div className="content">
+          {/* SWITCH has been replaced with ROUTES */}
           <Switch>
             <Route exact path="/">
               <Home />
             </Route>
             <Route exact path="/create">
               <Create />
+            </Route>
+            <Route exact path="/blogs/:id">
+              <BlogDetails />
             </Route>
           </Switch>
         </div>
--- react-blog/src/components/BlogList.jsx
+++ react-blog/src/components/BlogList.jsx
@@ -1,13 +1,17 @@
 /* eslint-disable react/prop-types */
+import { Link } from "react-router-dom";
+
 const BlogList = ({ blogs, title }) => {
   return (
     <div className="blog-list">
       <h2>{title}</h2>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
-          <h2>{blog.title}</h2>
-          <p>Authored by: {blog.author}</p>
-          {/* <button onClick={() => handleDelete(blog.id)}>Delete</button> */}
+          <Link to={`/blogs/${blog.id}`}>
+            <h2>{blog.title}</h2>
+            <p>Authored by: {blog.author}</p>
+            {/* <button onClick={() => handleDelete(blog.id)}>Delete</button> */}
+          </Link>
         </div>
       ))}
     </div>
